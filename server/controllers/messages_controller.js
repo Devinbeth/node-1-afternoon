7eb38dade37bcdd39fd5368786bb33a7dcbdaba9
Devin Lethbridge <devinbeth@gmail.com>
2018-02-07 16:20:39 -0700

    everything works!

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,31 @@
+let messages = []
+let id = 0
+
+module.exports = {
+    create: (req, res) => {
+        req.body.id = id
+        messages.push(req.body)
+        id++
+        res.status(200).send(messages)
+    },
+    read: (req, res) => {
+        res.status(200).send(messages)
+    },
+    update: (req, res) => {
+        for (let i = 0; i < messages.length; i++) {
+            if (messages[i].id === +req.params.id) {
+                messages[i].text = req.body.text || messages[i].text
+                messages[i].time = req.body.time || messages[i].time
+                return res.status(200).send(messages)
+            }
+        }
+    },
+    delete: (req, res) => {
+        for (let i = 0; i < messages.length; i++) {
+            if (messages[i].id === +req.params.id) {
+                messages.splice(i, 1)
+                return res.status(200).send(messages)
+            }
+        }
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mc = require('./controllers/messages_controller.js')
+const app = express()
+app.use(bodyParser.json())
+app.use(express.static('../public/build'))
+
+const url = '/api/messages'
+app.post(url, mc.create)
+app.get(url, mc.read)
+app.put(`${url}/:id`, mc.update)
+app.delete(`${url}/:id`, mc.delete)
+
+const port = 3000
+app.listen(port, () => console.log(`Server is listening at ${port}`))
