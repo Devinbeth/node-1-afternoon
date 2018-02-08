b0ffea0c00049711d1aa78bd80908e9a19bd0ebb
Devin Lethbridge <devinbeth@gmail.com>
2018-02-08 10:09:20 -0700

    semicolons

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mc = require('./controllers/messages_controller.js')
-const app = express()
-app.use(bodyParser.json())
-app.use(express.static('../public/build'))
+/* jshint node: true */
+const express = require("express");
+const bodyParser = require("body-parser");
+const mc = require("./controllers/messages_controller.js");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("../public/build"));
 
-const url = '/api/messages'
-app.post(url, mc.create)
-app.get(url, mc.read)
-app.put(`${url}/:id`, mc.update)
-app.delete(`${url}/:id`, mc.delete)
+const url = "/api/messages";
+app.post(url, mc.create);
+app.get(url, mc.read);
+app.put(`${url}/:id`, mc.update);
+app.delete(`${url}/:id`, mc.delete);
 
-const port = 3000
-app.listen(port, () => console.log(`Server is listening at ${port}`))
+const port = 3000;
+app.listen(port, () => console.log(`Server is listening at ${port}`));
